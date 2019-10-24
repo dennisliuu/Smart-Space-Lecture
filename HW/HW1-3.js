29c8ee36e791b4d9ae2eef45d07c83460891a308
dennisliuu <dennisliuu@gmail.com>
2019-10-24 22:10:28 +0800

    update homework

--- HW/HW1-3.js
+++ HW/HW1-3.js
@@ -1,8 +1,9 @@
-let deadline = 20351231
-let today = 20360101
-let disability = false
-let is_dead = false
+// let a = "單槓", b = "鋼彈"
+// console.log(a + "盪" + a);
+// console.log(b + "盪" + b);
+// console.log(a + "盪" + b);
 
-if (disability || is_dead || today > deadline)
-    console.log(100 M GET!);
-    
+let a = "湯"
+let b = "塔"
+console.log("和尚端" + a + "上" + b + "，" + b + "滑湯燙，" + a + "趟塔");
+console.log("和尚端" + b + "上" + a + "，" + a + "滑" + b + "燙，" + b + "燙" + a)
